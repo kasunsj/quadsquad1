001247f135d579a9dcd5bd68010e77fe717ca9b6
kasunsj <Dell@Dell-PC>
2016-09-06 09:50:26 +0530

    aligning

--- ITC515_assignment2/src/datamanagement/Student.java
+++ ITC515_assignment2/src/datamanagement/Student.java
@@ -1,19 +1,25 @@
 package datamanagement;
 
-public class Student implements IStudent {
-    private Integer id; private String fn;
-            private String ln;
-private StudentUnitRecordList su;
+public class Student implements IStudent 
+{
+	private Integer id;
+	private String fn;
+    private String ln;
+    private StudentUnitRecordList su;
 
-public Student( Integer id, String fn, String ln, StudentUnitRecordList su ) { this.id = id; this.fn = fn;
+    public Student( Integer id, String fn, String ln, StudentUnitRecordList su )
+    { 
+    	this.id = id; this.fn = fn;
         this.ln = ln;this.su = 
         su == null ? new StudentUnitRecordList() : 
-                su;
-}
-
+        su;
+    }
     public Integer getID() { return this.id; 
-} public String getFirstName() { 
-return fn; }
+} 
+public String getFirstName() 
+	{ 
+		return fn; 
+	}
 
     public void setFirstName( String firstName ) { 
 this.fn = firstName; }
